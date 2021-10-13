8472d558caf32dc1ec2e68dcc28d0fb0c8277b6b
Manas-254 <manaswinijain728@gmail.com>
2021-10-13 19:25:40 +0530

    adding all files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      checkedData: [],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c502c;
+  margin-top: 60px;
+  background-color: lightgoldenrodyellow;
+}
+</style>
